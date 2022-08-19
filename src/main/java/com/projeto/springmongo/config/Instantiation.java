73a505eabfeb6502a9f0bfe338bebb97f3a7f102
João Marcos <joaomarcosjmos.ans@gmail.com>
2022-08-19 16:06:10 -0300

    Acrescentando comentário aos posts

--- src/main/java/com/projeto/springmongo/config/Instantiation.java
+++ src/main/java/com/projeto/springmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.projeto.springmongo.config;
 import com.projeto.springmongo.domain.Post;
 import com.projeto.springmongo.domain.User;
 import com.projeto.springmongo.dto.AuthorDTO;
+import com.projeto.springmongo.dto.CommentDTO;
 import com.projeto.springmongo.repository.PostRepository;
 import com.projeto.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,19 +26,26 @@ public class Instantiation implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        var sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
         postRepository.deleteAll();
 
-        User maria = new User(null, "Maria Brown", "maria@gmail.com");
-        User alex = new User(null, "Alex Green", "alex@gmail.com");
-        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        var maria = new User(null, "Maria Brown", "maria@gmail.com");
+        var alex = new User(null, "Alex Green", "alex@gmail.com");
+        var bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria,alex, bob));
 
-        Post post1 =new Post(null, sdf.parse("20/05/2022"), "Bom dia", "Acordei muito bem", new AuthorDTO(maria));
-        Post post2 =new Post(null, sdf.parse("30/01/2022"), "Boa noite", "Fui dormir muito bem", new AuthorDTO(maria));
+        var post1 =new Post(null, sdf.parse("20/05/2022"), "Bom dia", "Acordei muito bem", new AuthorDTO(maria));
+        var post2 =new Post(null, sdf.parse("30/01/2022"), "Boa noite", "Fui dormir muito bem", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("30/05/2022"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveitem!", sdf.parse("310/04/2022"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("30/05/2022"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/projeto/springmongo/domain/Post.java
+++ src/main/java/com/projeto/springmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.projeto.springmongo.domain;
 
 import com.projeto.springmongo.dto.AuthorDTO;
+import com.projeto.springmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,8 @@ public class Post {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
+
     public Post(){
     }
 
@@ -26,6 +31,14 @@ public class Post {
         this.title = title;
         this.body = body;
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     public String getId() {
